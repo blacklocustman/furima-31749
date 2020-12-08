9ef411d3c6265200725f9b8678e3109bfe30e422
blacklocustman <on87.yuzawa.yuuta@gmail.com>
2020-12-08 13:43:59 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    binding.pry
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
   end
 end
