012700c88ff6d91ddc02d345f5e252aab3841665
blacklocustman <on87.yuzawa.yuuta@gmail.com>
2020-12-13 23:31:42 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true      
+  validates :nickname, presence: true
+  validates :password, presence: true, length: { maximum: 6 }   
 end
